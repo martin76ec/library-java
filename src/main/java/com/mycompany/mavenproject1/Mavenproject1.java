8281cdbf0f9573f790e3f1d207284047bdfb846e
martin76ec <larreaamartin@gmail.com>
2023-08-05 12:30:04 -0500

    main commit

---
+++ src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject1;
+
+import javax.swing.JFrame;
+import ui.Main;
+
+/**
+ *
+ * @author martin
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        JFrame main = new Main();
+        main.show();
+    }
+}
